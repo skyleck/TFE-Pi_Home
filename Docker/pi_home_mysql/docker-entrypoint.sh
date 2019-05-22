1b8c2b806e1d2118063188029aa054f5643aac63
skyleck <maximehempte@gmail.com>
2019-05-22 15:13:53 +0100

    Update Docker mysql

--- Docker/pi_home_mysql/docker-entrypoint.sh
+++ Docker/pi_home_mysql/docker-entrypoint.sh
@@ -1,20 +1,19 @@
 #!/bin/bash 
-set -e 
+set -e
 
 service mysql start
 
-mysql -u root --password=password -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('"$MYSQL_ROOT_PASSWORD"');"
+mysql -u root --password=password -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('"test"');"
 
-mysql -u root --password="$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE pi_home;"
-mysql -u root --password="$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE pi_home_test;"
+mysql -u root --password="test" -e "CREATE DATABASE pi_home;"
+mysql -u root --password="test" -e "CREATE DATABASE pi_home_test;"
 
 for f in /docker_entrypoint-initdb.d/*; do
-	mysql -u root --password="$MYSQL_ROOT_PASSWORD" < $f
+	mysql -u root --password="test" < $f
 done
 
-sed -e "s/^bind-address\(.*\)=.*/bind-address=0.0.0.0/" -i /etc/mysql/my.cnf
-mysql -u root --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '"$MYSQL_ROOT_PASSWORD"' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+sed -e "s/^bind-address\(.*\)=.*/bind-address=0.0.0.0/" -i /etc/mysql/mariadb.conf.d/50-server.cnf
 
-service mysql stop
+mysql -u root --password="test" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'test' WITH GRANT OPTION;"
 
 exec "$@"
