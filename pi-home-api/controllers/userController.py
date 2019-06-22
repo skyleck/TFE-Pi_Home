5270f7b4915b29810897cedfa56ad82bba7bf61a
skyleck <maximehempte@gmail.com>
2019-06-22 23:08:59 +0200

    Refactor

--- pi-home-api/controllers/userController.py
+++ pi-home-api/controllers/userController.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
 import sys
 sys.path.append("pi_home_core")
 
@@ -10,59 +12,18 @@ from src.be.infokea.pi_home.utils.UseDB import UseDB
 from src.be.infokea.pi_home.domain.User import User
 from flask_cors import CORS, cross_origin
 
-userImpl = UserImpl(UseDB("192.168.1.155","root","test","pi_home"))
+from LoginRessource import LoginRessource
+from UserResource import UserResource
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
 cors = CORS(app, support_credentials=True,resources={r"/api/*": {"origins": "*"}})
+api = Api(app)
 
-@app.route('/api/getAllUsers')
-def getAllUsers():
-    users = userImpl.selectAll()
-    return jsonify([e.jsonFormat() for e in users])
+jwt = JWTManager(app)
 
-@app.route('/api/getUser')
-def getUser():
-    return "ok"
-    
-@app.route('/api/login', methods = ['POST'])
-def login():
-    content = request.get_json()
-    print(content)
-    user = userImpl.login(content["login"],content["password"])
-    print(user)
-    if(user == None):
-        response = jsonify('Login or password incorrect')
-        response.status_code = 400
-        return response
-    return jsonify([e.jsonFormat() for e in user])
-
-@app.route('/addUser', methods = ['POST'])
-def createUser():
-    content = request.get_json()
-    print (content["firstname"])
-    userImpl.insert(User(content["login"],content["firstname"],content["lastname"],content["password"]))
-    return "ok"
-
-@app.route('/', methods = ['POST'])
-def test():
-    print(request.headers)
-    print(request.data)
-    print(request.form.to_dict())
-    print(request.files)
-    return "ok"
-
-@app.route("/updateUser", methods = ['PUT'])
-def updateUser():
-    content = request.get_json()
-    user = User(content["login"],content["firstname"],content["lastname"],content["password"])
-    print(userImpl.updateUser(content["id"], user))
-    return "User updated"
-
-@app.route("/deleteUser", methods = ['DELETE'])
-def deleteUser():
-    content = request.get_json()
-    userImpl.delete(content["id"])
-    return "User delete"
+api.add_resource(LoginRessource, '/api/login')
+api.add_resource(UserResource, "/api/user", "/api/user/<id>")
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port=5001)
--- pi_home_core/src/be/infokea/pi_home/componentImpl/user/UserImpl.py
+++ pi_home_core/src/be/infokea/pi_home/componentImpl/user/UserImpl.py
@@ -1,88 +1,149 @@
 from src.be.infokea.pi_home.componentImpl.IData import IData
 from src.be.infokea.pi_home.domain.User import User
 import pymysql
+import hashlib
 
 class UserImpl():
 
-    def __init__(self, useDb):
-        self.table = "user"
-        self.columns = ["login","firstname", "lastname","password"]
-        self.columnsParams = ["id INTEGER AUTO_INCREMENT", "login varchar(25) NOT NULL" "firstname varchar(30) NOT NULL", "lastname varchar(30) NOT NULL", "password varchar(30) NOT NULL", "PRIMARY KEY(id)"]
-
-        self.useDb = useDb
-        self.db = pymysql.connect("192.168.1.155","root","test","pi_home")
-        self.cursor = self.db.cursor()
-        #self.cursor = self.getDBCursor()
+    def __init__(self):
+        self.ip = "192.168.1.155"
+        self.user = "root"
+        self.password = "test"
+        self.database = "pi_home"
 
     def getDBCursor(self):
         db = pymysql.connect("192.168.1.155","root","test","pi_home")
         cursor = db.cursor()
 
-    def constructValues(self,user):
-        return (user.getLogin(), user.getFirstname(), user.getLastname(), user.getPassword())
-
-    def createTable(self):
-        self.useDb.createTable(self.table,self.columnsParams)
-
-    def dropTable(self):
-        self.useDb.dropTable(self.table)
-
     def login(self,login,password):
-        sql = "SELECT * FROM user where login = '"+ login + "' AND password = '" + password + "'"
-        self.cursor.execute(sql)
-        results = self.cursor.fetchall()
-        if(len(results) == 0):
-            return None;
-        else:
+        sql = "SELECT * FROM user where login = '"+ login + "' AND password = sha2('" + password + "salt',512)"
+        db = pymysql.connect(self.ip,self.user,self.password,self.database)
+        try:
+            cursor = db.cursor()
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            if(len(results) == 0):
+                return None;
+            else:
+                users = []
+                for row in results:
+                    id = row[0]
+                    login = row[1]
+                    firstname = row[2]
+                    lastname = row[3]
+                    password = row[4]
+                    authorization = row[5]
+                    user = User(id, login, firstname, lastname, password,authorization)
+                    users.append(user)
+                return users
+        finally:
+            db.close()
+
+    def getUserByLogin(self,login):
+        sql = "SELECT * FROM user where login = '" + login + "'"
+        db = pymysql.connect(self.ip,self.user,self.password,self.database)
+        try:
+            cursor = db.cursor()
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            if(len(results) == 0):
+                return None
+            else:
+                users = []
+                for row in results:
+                    id = row[0]
+                    login = row[1]
+                    firstname = row[2]
+                    lastname = row[3]
+                    password = row[4]
+                    authorization = row[5]
+                    user = User(id, login, firstname, lastname, password,authorization)
+                    users.append(user)
+                return users
+        finally:
+            db.close()
+
+    def insert(self,user):
+        sql = "INSERT INTO user (login,firstname,lastname,password,authorization) VALUES ('"+ user.getLogin() + "','"  \
+                                                                    + user.getFirstname() + "','" \
+                                                                    + user.getLastname() + "'," \
+                                                                    + "sha2('" + user.getPassword() + "salt',512)," \
+                                                                    + str(user.getAuthorization()) + ")"
+        db = pymysql.connect(self.ip,self.user,self.password,self.database)
+        try:
+            cursor = db.cursor()
+            cursor.execute(sql)
+            db.commit()
+        finally:
+            db.close()
+
+    def select(self,id):
+        sql = "SELECT * FROM user WHERE id = " + id
+        db = pymysql.connect(self.ip,self.user,self.password,self.database)
+        try:
+            cursor = db.cursor()
+            cursor.execute(sql)
+            results = cursor.fetchall()
             users = []
             for row in results:
+                id = row[0]
                 login = row[1]
                 firstname = row[2]
                 lastname = row[3]
                 password = row[4]
-                user = User(login, firstname, lastname, password)
+                authorization = row[5]
+                user = User(id,login, firstname, lastname, password,authorization)
                 users.append(user)
             return users
-    
-    def insert(self,element):
-        self.useDb.insert(self.table, self.columns, self.constructValues(element))
-
-    def select(self,id):
-        usersDB = self.useDb.select(self.table,id)
-        users = []
-        for row in usersDB:
-            login = row[1]
-            firstname = row[2]
-            lastname = row[3]
-            password = row[4]
-            user = User(login, firstname, lastname, password)
-            users.append(user)
-        return users
+        finally:
+            db.close()
 
     def selectAll(self):
-        usersDB = self.useDb.selectAll(self.table)
-        users = []
-        for row in usersDB:
-            login = row[1]
-            firstname = row[2]
-            lastname = row[3]
-            password = row[4]
-            user = User(login, firstname, lastname, password)
-            users.append(user)
-        return users
+        sql = "SELECT * FROM user"
+        db = pymysql.connect(self.ip,self.user,self.password,self.database)
+        try:
+            cursor = db.cursor()
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            users = []
+            for row in results:
+                id = row[0]
+                login = row[1]
+                firstname = row[2]
+                lastname = row[3]
+                password = row[4]
+                authorization = row[5]
+                user = User(id,login, firstname, lastname, password,authorization)
+                users.append(user)
+            return users
+        finally:
+            db.close()
     
     def updateUser(self,id,user):
-        newValues = [user.getLogin(),user.getFirstname(),user.getLastname(),user.getPassword()]
-        self.useDb.update(self.table,self.columns,newValues,id)
+        sql = "UPDATE user SET login ='" + user.getLogin() + "'" \
+                            + " , firstname ='" + user.getFirstname() + "'" \
+                            + " , lastname = '" + user.getLastname() + "'" \
+                            + " , password = " + "sha2('" + user.getPassword() + "salt',512)" \
+                            + " , authorization = " + str(user.getAuthorization()) \
+                            + " WHERE id = " + str(id)
 
-    def updateFirstname(self,oldValue,newValue,lastname):
-        self.useDb.update(self.table,"firstname",newValue,["firstname","lastname"],[oldValue,lastname])
-   
-    def updateLastname(self,oldValue,newValue,firstname):
-        self.useDb.update(self.table,"lastname",newValue,["firstname","lastname"],[firstname,oldValue])
+        db = pymysql.connect(self.ip,self.user,self.password,self.database)
+        try:
+            cursor = db.cursor()
+            cursor.execute(sql)
+            db.commit()
+        finally:
+            db.close()
     
     def delete(self,id):
-        self.useDb.delete(self.table,id)
+        sql = "DELETE FROM user where id = " + str(id)
+        db = pymysql.connect(self.ip,self.user,self.password,self.database)
+        try:
+            cursor = db.cursor()
+            cursor.execute(sql)
+            db.commit()
+        finally:
+            db.close()
         
     def close(self):
         self.useDb.close()
